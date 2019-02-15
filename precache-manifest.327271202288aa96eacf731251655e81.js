eb57d0fbc5fb1d0ae8a2d5842e561e685063b848
Ludwig J <35335110+LudwigJedde@users.noreply.github.com>
2019-02-15 15:40:33 +0100

    initial commit

---
+++ precache-manifest.327271202288aa96eacf731251655e81.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "760a9b9c5fe49ed9d23d",
+    "url": "/static/js/main.760a9b9c.chunk.js"
+  },
+  {
+    "revision": "655ae23ef92797b73f2b",
+    "url": "/static/js/2.655ae23e.chunk.js"
+  },
+  {
+    "revision": "6c10cc75aec0e6e14dba3db0d10aa6b9",
+    "url": "/index.html"
+  }
+];
